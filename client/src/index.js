5a1f28aae6b9299f68631f033f5ffd305f778050
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-02-04 21:39:22 +0100

    adds disable-react-devtools

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
